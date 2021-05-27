edf2fb0dcd95915408c28a3680395db1f79dbb79
Anneke Schütte <a.schuette.hh@gmail.com>
2021-05-28 00:17:59 +0200

    Check for brick collision

--- src/Collision.ts
+++ src/Collision.ts
@@ -1,9 +1,42 @@
 // Types
 import { Paddle } from './sprites/Paddle'
 import { Ball } from './sprites/Ball'
+import { Brick } from './sprites/Brick'
 import { CanvasView } from './view/CanvasView'
 
 export class Collision {
+
+  isCollidingBrick(ball: Ball, brick: Brick): boolean {
+    if (
+      ball.pos.x < brick.pos.x + brick.width &&
+      ball.pos.x + ball.width > brick.pos.x &&
+      ball.pos.y < brick.pos.y + brick.height &&
+      ball.pos.y + ball.height > brick.pos.y
+    ) {
+      return true
+    } else {
+      return false
+    }
+  }
+  // check ball collision with bricks
+  isCollidingBricks(ball: Ball, bricks: Brick[]): boolean {
+    let colliding = false
+
+    bricks.forEach((brick, i) => {
+      if (this.isCollidingBrick(ball, brick)) {
+        ball.changeYDirection()
+
+        if (brick.energy === 1) {
+          bricks.splice(i, 1)
+        } else {
+          brick.energy -= 1
+        }
+        colliding = true
+      }
+    })
+    return colliding
+  }
+
   checkBallCollision(ball: Ball, paddle: Paddle, view: CanvasView):void {
     // check ball collision with paddle
     if (
